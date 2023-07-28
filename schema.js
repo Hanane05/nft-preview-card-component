db09ee305408d7ade7c410aa752a4405c2e758e9
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2023-07-28 11:58:49 -0400

    Adding main tag to index.html

---
+++ schema.js
@@ -0,0 +1,101 @@
+{
+    "@context": "https://schema.org",
+        "@graph": [
+            {
+                "@type": "Organization",
+                "@id": "https://bdhcollective.com/#organization",
+                "name": "My WordPress",
+                "logo": {
+                    "@type": "ImageObject",
+                    "@id": "https://bdhcollective.com/#logo",
+                    "url": "https://bdhcollective.com/wp-content/uploads/2023/04/BDH_LOGO-011-240-x-120-150x120.png",
+                    "contentUrl": "https://bdhcollective.com/wp-content/uploads/2023/04/BDH_LOGO-011-240-x-120-150x120.png",
+                    "caption": "My WordPress",
+                    "inLanguage": "en-US"
+                }
+            },
+            {
+                "@type": "WebSite",
+                "@id": "https://bdhcollective.com/#website",
+                "url": "https://bdhcollective.com",
+                "name": "My WordPress",
+                "publisher": {
+                    "@id": "https://bdhcollective.com/#organization"
+                },
+                "inLanguage": "en-US"
+            },
+            {
+                "@type": "ImageObject",
+                "@id": "https://bdhcollective.com/wp-content/uploads/2023/01/BDH-Collective-internship-job-intern-Montral-Toronto-Agency-Marketing-Public-relation-Production-Stagiaire-EN2-1.jpg",
+                "url": "https://bdhcollective.com/wp-content/uploads/2023/01/BDH-Collective-internship-job-intern-Montral-Toronto-Agency-Marketing-Public-relation-Production-Stagiaire-EN2-1.jpg",
+                "width": 1024,
+                "height": 1024,
+                "inLanguage": "en-US"
+            },
+            {
+                "@type": "WebPage",
+                "@id": "https://bdhcollective.com/recruitment/it-program-information-technologies/#webpage",
+                "url": "https://bdhcollective.com/recruitment/it-program-information-technologies/",
+                "name": "IT Program Intern - Gain Valuable Experience - BDH Collective",
+                "datePublished": "2023-06-09T01:01:00+00:00",
+                "dateModified": "2023-06-13T15:32:21+00:00",
+                "isPartOf": {
+                    "@id": "https://bdhcollective.com/#website"
+                },
+                "primaryImageOfPage": {
+                    "@id": "https://bdhcollective.com/wp-content/uploads/2023/01/BDH-Collective-internship-job-intern-Montral-Toronto-Agency-Marketing-Public-relation-Production-Stagiaire-EN2-1.jpg"
+                },
+                "inLanguage": "en-US"
+            },
+            {
+                "@type": "JobPosting",
+                "title": "IT Program Intern",
+                "description": "Join our Montreal/Toronto creative agency as an IT Program Intern. Gain valuable experience in information and library technologies.",
+                "baseSalary": {
+                    "@type": "MonetaryAmount",
+                    "currency": "CAD",
+                    "value": {
+                        "@type": "QuantitativeValue",
+                        "value": "0"
+                    }
+                },
+                "datePosted": "2023-06-09",
+                "employmentType": [
+                    "INTERN"
+                ],
+                "hiringOrganization": {
+                    "@type": "Organization",
+                    "name": "BDH Collective",
+                    "sameAs": "https://bdhcollective.com"
+                },
+                "jobLocationType": "TELECOMMUTE",
+                "applicantLocationRequirements": [
+                    {
+                        "@type": "Country",
+                        "name": "canada"
+                    }
+                ],
+                "jobLocation": {
+                    "@type": "Place",
+                    "address": {
+                        "@type": "PostalAddress",
+                        "streetAddress": "700-407 rue McGill",
+                        "addressLocality": "Montreal",
+                        "addressRegion": "Quebec",
+                        "postalCode": "H2Y 2G3",
+                        "addressCountry": "Canada"
+                    }
+                },
+                "experienceInPlaceOfEducation": "False",
+                "educationRequirements": [],
+                "image": {
+                    "@type": "ImageObject",
+                    "url": "https://bdhcollective.com/wp-content/uploads/2023/01/BDH-Collective-internship-job-intern-Montral-Toronto-Agency-Marketing-Public-relation-Production-Stagiaire-EN2-1.jpg"
+                },
+                "@id": "https://bdhcollective.com/recruitment/it-program-information-technologies/#schema-156521",
+                "mainEntityOfPage": {
+                    "@id": "https://bdhcollective.com/recruitment/it-program-information-technologies/#webpage"
+                }
+            }
+        ]
+}
